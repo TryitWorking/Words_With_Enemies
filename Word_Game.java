338066dca5f01fa0c0561df2b4b0f7158f3891ca
benzhen <bluehelper358@gmail.com>
2015-02-02 18:50:08 -0500

    First Iteration
    Reddit Challenge #198

---
+++ Word_Game.java
@@ -0,0 +1,79 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+//Reddit Daily Challenge #198 Words With Enemies
+public class Word_Game {
+
+	public static void main(String[] arg)
+	{
+		System.out.println("Game is running");
+		
+		String firstWord;
+		String secondWord;
+		ArrayList<String> find = new ArrayList<String>();
+		Scanner words = new Scanner(System.in);
+		boolean play = true;
+		
+		while(play){
+			System.out.println("Game Start");
+			System.out.println("Enter in two words, seperated by a space");
+			firstWord = words.next();
+			secondWord = words.next();
+			
+			//System.out.println(firstWord + "," + secondWord);
+			//System.out.println(firstWord.length() + "," + secondWord.length());
+			
+			if((firstWord.length() != 0 || firstWord != null) && (secondWord.length() !=0 || secondWord != null))
+			{
+				for(int ii=0; ii<firstWord.length();ii++)
+				{
+					for(int jj=0; jj<secondWord.length();jj++)
+					{
+						if(firstWord.charAt(ii) == secondWord.charAt(jj))
+						{
+							find.add("" + firstWord.charAt(ii));
+						}
+					}
+				}
+				
+				//System.out.println("ArrayList Size: " + find.size());
+				
+					
+				for(int ii=0; ii<find.size(); ii++)
+				{
+					if(firstWord.contains(find.get(ii)) && secondWord.contains(find.get(ii)))
+					{
+						//System.out.println("found" + ", " + find.get(ii));
+						firstWord = firstWord.replaceFirst(find.get(ii), "");
+						secondWord = secondWord.replaceFirst(find.get(ii), "");
+					}
+				}
+				
+				System.out.println(firstWord + "," + secondWord);
+				//System.out.println(firstWord.length() + "," + secondWord.length());
+				
+				if(firstWord.length() > secondWord.length())
+				{
+					System.out.println("Left Side Wins!!!, " + firstWord.length() + " to " + secondWord.length());
+				}
+				else if(secondWord.length() > firstWord.length())
+				{
+					System.out.println("Right Side Wins!!!, " + firstWord.length() + " to " + secondWord.length());
+				}
+				else
+				{
+					System.out.println("Tie, Try Again, " + firstWord.length() + " to " + secondWord.length());
+				}
+			
+				find.clear();
+				firstWord = "";
+				secondWord = "";
+			}
+			else
+			{
+				System.out.println("Two words have to be inputted!");
+			}
+		}
+	}
+	
+}
